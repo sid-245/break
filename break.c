f282b2741680b7f4157a997a5519224d531670f8
Siddhi@patil <118968670+sid-245@users.noreply.github.com>
2023-01-06 13:52:00 +0530

    Add files via upload

---
+++ break.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int i;
+    for(i=1;i<=50;i++)
+    {
+      if(i%5==0)
+      {
+        break;
+       }
+      printf("\n%d",i);
+      
+    }
+    return 0;
+ }    
